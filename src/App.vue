f899adf03a983a668df02edc39a1761348a39491
jgreenlee26 <justin.greenlee@rewardstyle.com>
2022-12-05 08:50:12 -0600

    :ta-da: init project!

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useAuthUserStore } from "./stores/AuthUserStore";
+
+const authUserStore = useAuthUserStore();
+authUserStore.initUser();
+</script>
+
+<template>
+  <Navbar />
+  <Drawer />
+  <Alerts />
+</template>
